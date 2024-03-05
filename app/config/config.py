617990e750f875e1a74e95a168a908f9c35c7b2c
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-03-05 17:11:35 +0700

    Added option to use .env loading from pydantic

--- app/config/config.py
+++ app/config/config.py
@@ -1,10 +1,12 @@
 from functools import lru_cache
 from pathlib import Path
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings  #, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    #model_config = SettingsConfigDict(env_file="app/.env", env_file_encoding="utf-8")
+
     jwt_secret_key: str
     algorithm: str
     access_token_expire_minutes: int
